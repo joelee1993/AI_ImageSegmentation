10328bbdaa99766ecd0fc7c80cd6d064a0ddaaef
j217435 <78093668+j217435@users.noreply.github.com>
2023-01-28 21:36:24 -0500

    Update and rename unet_BCE_module.sh to model/unet_BCE_module.sh

--- model/unet_BCE_module.sh
+++ model/unet_BCE_module.sh
@@ -6,9 +6,6 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=j217435@gmail.com
 
-module load anaconda 
-conda create -n para_comunet 
-module activate para_comuent
 module load pytorch-extra-py37-cuda10.2-gcc8/1.8.1 pytorch-py37-cuda10.2-gcc8/1.8.1 pytorch-py37-cuda10.2-gcc/1.6.0
 module load python37
 module load nvhpc
@@ -17,10 +14,10 @@ pip install simpleitk
 python -c"import torch; device = torch.device('cuda:0' if torch.cuda.is_available() else'cpu'); print(device)"
 
 python UNet3d_para_BCEonehot1208.py --mode "train" \
-                --model_save_name "para_comunet_BCEModule"\
-                --train_data_dir "/projects/AIS-CT/Test/images/imagesTr" \
-                --label_data_dir "/projects/AIS-CT/Test/images/labelsTr" \
-		    --model_save_dir "/projects/AIS-CT/Test/Unet/model_saved"\
+                --model_save_name ""\
+                --train_data_dir "" \
+                --label_data_dir "" \
+		    --model_save_dir ""\
                 --lr 0.001 \
                 --epochs 50\
                 --bs 4\
